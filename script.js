ec90d25cdf6f4c2b1e25d8deaa0ea2386e7d5f0d
Salfi Sabit <70028517+Salf1-Sabit@users.noreply.github.com>
2023-12-16 22:09:41 +0600

    Concluded - Conquering Responsive Layout Final Challenge

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleMenu() {
+  let navLinks = document.querySelector(".mobile__nav__links");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+}
